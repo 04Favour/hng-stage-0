92c4597cc1b353e652abd26e983a21dd91ecf65b
Favour Mfon <favourmichael004@gmail.com>
2026-04-16 11:58:40 +0100

    add assertion for nameDto

--- src/classify.dto.ts
+++ src/classify.dto.ts
@@ -3,5 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class NameDto {
   @IsString({ message: 'Name must be a string' })
   @IsNotEmpty({ message: 'Name is required' })
-  name: string;
+  name!: string;
 }
